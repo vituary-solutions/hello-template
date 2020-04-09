273de55723f84c597ac2a6d010357161894cf377
Justin Guertin <jguertin@vituary.com>
2020-04-08 22:11:10 -0700

    Initial commit of a "Hello World" react application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# hello-template
-A template project for full project including deployments
+# Hello Template
+A template project for react project
+
+## Get Started
+
+### React UI
+
+A Web based front end
+
+`$ cd react-ui`
+
+See [react-ui](./react-ui/README.md) for details.
---
+++ react-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo-vertical-410w.png" className="App-logo" alt="logo" />
+        Hello World!
+      </header>
+    </div>
+  );
+}
+
+export default App;
