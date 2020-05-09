d867fd26d44bef73ad6ae644c84ed5901aafe4c0
Justin Guertin <jguertin@vituary.com>
2020-05-08 23:47:16 -0700

    Corrections to workflows and test configuration

--- react-ui/src/components/__tests__/Greeting.test.js
+++ react-ui/src/components/__tests__/Greeting.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { render, waitForDomChange } from '@testing-library/react';
 import Greeting from '../Greeting';
-import {getGreetingName} from "../../service/hello-service";
-import {useAuth0} from "../../react-auth0-spa";
+import {useAppContext} from "../../context/application-context";
 
-jest.mock('../../service/hello-service')
-jest.mock('../../react-auth0-spa')
+jest.mock('../../context/application-context')
 
 test('renders resolved greeting', async () => {
+  const getGreetingName = jest.fn()
   getGreetingName.mockResolvedValue(Promise.resolve("World"))
-  useAuth0.mockImplementation(() => ({isAuthenticated: true, getTokenSilently: () => Promise.resolve("Token")}))
+  useAppContext.mockImplementation(() => ({isAuthConfigured: true, helloService: {getGreetingName}}))
   const { getByText } = render(<Greeting />);
   await waitForDomChange(getByText)
   const textElement = getByText(/Hello World/i);
@@ -17,7 +16,7 @@ test('renders resolved greeting', async () => {
 });
 
 test('renders guest by default', async () => {
-  useAuth0.mockImplementation(() => ({isAuthenticated: false}))
+  useAppContext.mockImplementation(() => ({isAuthConfigured: false}))
   const { getByText } = render(<Greeting />);
   const textElement = getByText(/Hello Guest/i);
   expect(textElement).toBeInTheDocument();
