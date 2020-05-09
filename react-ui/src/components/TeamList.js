fede5556df98e415f99c8845dda124ec85209b87
Justin Guertin <jguertin@vituary.com>
2020-05-09 11:42:09 -0700

    Improve error handling for teams and add default environment variables to the Dockerfile

--- react-ui/src/components/TeamList.js
+++ react-ui/src/components/TeamList.js
@@ -11,16 +11,21 @@ export default function TeamList() {
     }
 
     const loadTeams = async () => {
-      const response = await teamService.getTeams()
-      setTeams(response.sort((a, b) => {
-        if ( a.name < b.name ){
-          return -1;
-        }
-        if ( a.name > b.name ){
-          return 1;
-        }
-        return 0;
-      }))
+      try {
+        const response = await teamService.getTeams()
+        setTeams(response.sort((a, b) => {
+          if ( a.name < b.name ){
+            return -1;
+          }
+          if ( a.name > b.name ){
+            return 1;
+          }
+          return 0;
+        }))
+      }
+      catch (e) {
+        console.error(`Failed to load teams from service: ${e}`)
+      }
     }
     loadTeams()
   }, [isAuthConfigured]) // eslint-disable-line react-hooks/exhaustive-deps
